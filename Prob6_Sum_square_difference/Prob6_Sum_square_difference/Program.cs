cb539669cbebd96e63cefde26827229aeb888433
Anik Paul Gomes <gomesap29@gmail.com>
2018-09-23 16:54:00 -0500

    problem 6 started

---
+++ Prob6_Sum_square_difference/Prob6_Sum_square_difference/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Problem 6 
+
+Sum square difference
+
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob6_Sum_square_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        public static 
+    }
+}
