68f7261c54e15204e07279da38971e0925492277
Anik Paul Gomes <gomesap29@gmail.com>
2018-09-23 18:01:33 -0500

    problem 6 solved

--- Prob6_Sum_square_difference/Prob6_Sum_square_difference/Program.cs
+++ Prob6_Sum_square_difference/Prob6_Sum_square_difference/Program.cs
@@ -21,6 +21,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Prob6_Sum_square_difference
 {
@@ -28,10 +29,35 @@ namespace Prob6_Sum_square_difference
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum {0} ", (sumOfSquares(100) - squareOfSum(100)));
+            stopwatch.Stop();
+
+            Console.WriteLine("The project runtime: {0} ms", stopwatch.ElapsedMilliseconds);
+
 
         }
 
+        /*The formula for 1+2+3+....+n = n(n+1)/2
+         * The formula for 1^2 +2^2 + 3^2 + ...... +n^2 = n(2n+1)(n+1)/ 6
+         */
+         
 
-        public static 
+
+        public static long sumOfSquares(int n)
+        {
+            long sum = (n * (2 * n + 1) * (n + 1)) / 6;
+            return sum;
+        
+        }
+
+        public static long squareOfSum(int n)
+        {
+            long sum = (n * (n + 1)) / 2;
+            return sum * sum;
+ 
+        }
     }
 }
